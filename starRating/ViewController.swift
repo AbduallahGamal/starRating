93d834c99095ee756a3c3e03f323559f13ef3504
Abduallah <abduallahgamal@gmail.com>
2019-09-22 23:39:52 +0200

    Initial Commit

---
+++ starRating/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  starRating
+//
+//  Created by Abdalla on 9/22/19.
+//  Copyright © 2019 edu.data. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
